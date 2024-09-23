dc4b3d8b216fe0eeae42d2d882b3f2863a05e8a1
Matheus Henrique <matthpmoreira@gmail.com>
2024-09-23 03:42:10 -0300

    fix: hash passwords before storing

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,9 @@
+import bcrypt from "bcrypt";
 import http from "http-status";
+import jwt from "jsonwebtoken";
 import { createUser, isUserInDatabase } from "#db/userApi.js";
+
+const secret = process.env.JWT_SECRET;
 
 export async function signupController(req, res) {
     const user = req.body;
@@ -9,11 +13,11 @@ export async function signupController(req, res) {
             return res.sendStatus(http.CONFLICT);
         }
 
+        user.password = bcrypt.hashSync(user.password, 10);
         await createUser(user);
-        res.sendStatus(http.CREATED)
+        res.sendStatus(http.CREATED);
     } catch (err) {
         console.error(err);
         res.sendStatus(500);
     }
-
 }
