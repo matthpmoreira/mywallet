000dbb534faf79cbf2b16e7a462d559649840009
Matheus Henrique <matthpmoreira@gmail.com>
2024-09-24 02:14:06 -0300

    refactor: split transaction schemas

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,12 +1,12 @@
 import { deleteTransaction, getPaginatedTransactions, postTransaction, putTransaction } from "#controllers/transactionController.js";
 import { authMiddleware } from "#middlewares/authMiddleware.js";
 import { validationMiddleware } from "#middlewares/validationMiddleware.js";
-import { transactionSchema } from "#schemas/transactionSchema.js"
+import { newTransactionSchema, transactionIdSchema } from "#schemas/transactionSchema.js"
 import { Router } from "express";
 
 export const transactionRoutes = Router();
 transactionRoutes.use(authMiddleware);
-transactionRoutes.post("/transactions", validationMiddleware(transactionSchema), postTransaction);
+transactionRoutes.post("/transactions", validationMiddleware(newTransactionSchema), postTransaction);
 transactionRoutes.get("/transactions", getPaginatedTransactions);
-transactionRoutes.put("/transactions", validationMiddleware(transactionSchema), putTransaction);
-transactionRoutes.delete("/transactions", deleteTransaction)
+transactionRoutes.put("/transactions", validationMiddleware(transactionIdSchema), putTransaction);
+transactionRoutes.delete("/transactions", validationMiddleware(transactionIdSchema), deleteTransaction)
--- src/schemas/transactionSchema.js
+++ src/schemas/transactionSchema.js
@@ -1,9 +1,11 @@
 import joi from "joi";
 
-export const transactionSchema = joi.object({
+export const newTransactionSchema = joi.object({
     value: joi.number().positive().precision(2).required(),
     description: joi.string().required(),
-    type: joi.string().pattern(/^(deposit|withdraw)$/).required(),
-    userId: joi.string().optional(),
-    _id: joi.string().optional()
+    type: joi.string().pattern(/^(deposit|withdraw)$/).required()
 });
+
+export const transactionIdSchema = joi.object({
+    id: joi.string().required()
+})
